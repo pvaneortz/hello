e57b2dde5f82b8c21e659bf442c2347da08de50d
pvaneortz <46772447+pvaneortz@users.noreply.github.com>
2019-01-16 23:37:00 -0600

    hello
    hack#1

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Evan Portz
+ * Date: 2019/15/01
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Evan Portz\n");
+  printf("Computer Engineering\n");
+
+  return 0;
+}
